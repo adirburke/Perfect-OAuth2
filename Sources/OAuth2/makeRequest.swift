b85366c2ec01c48391ea2da5538ac574b88fb562
Adir Burke @ Work <zaraserver@dev.local>
2020-01-14 10:38:37 +1100

    Fixing issue with resId = -1 during import

--- Sources/OAuth2/makeRequest.swift
+++ Sources/OAuth2/makeRequest.swift
@@ -16,11 +16,44 @@ import Foundation
 
 import AsyncHTTPClient
 import NIOHTTP1
+import NIO
 
 extension OAuth2 {
     
     static let httpClient = HTTPClient(eventLoopGroupProvider: .createNew, configuration: .init(tlsConfiguration: nil, redirectConfiguration: nil, timeout: .init(), proxy: nil, ignoreUncleanSSLShutdown: true, decompression: .disabled))
 //    static let httpClient = HTTPClient(eventLoopGroupProvider: .createNew)
+    
+    static public func makeRequestNIO(_ method: NIOHTTP1.HTTPMethod,
+                                      _ url: String,
+                                      body: String = "",
+                                      encoding: String = "JSON",
+                                      bearerToken: String = "") throws -> EventLoopFuture<HTTPClient.Response> {
+        
+        var request = try HTTPClient.Request(url: url, method: method)
+        request.headers.add(name: "user-agent", value: "adir-test-app")
+        if !bearerToken.isEmpty {
+            request.headers.add(name: "Authorization", value: "Bearer \(bearerToken)")
+        }
+        request.body = .string(body)
+        
+        switch method {
+        case .POST:
+            if encoding == "form" {
+                request.headers.add(name: "Content-Type", value: "application/x-www-form-urlencoded")
+            } else
+            {
+                request.headers.add(name: "Content-Type", value: "application/json")
+            }
+            request.headers.add(name: "Content-length", value: "\(request.body?.length ?? 0)")
+        default:
+            break
+        }
+        //            request.body = nil
+        request.body = .string(body)
+        
+        return self.httpClient.execute(request: request)
+        
+    }
     
     static public func makeRequestNIO(
         _ method: NIOHTTP1.HTTPMethod,
@@ -220,91 +253,6 @@ extension OAuth2 {
         _ = semaphore.wait(timeout: OAuth2.timeOut)
         return returnValue
         
-        
-//        let curlObject = CURL(url: url)
-//        curlObject.setOption(CURLOPT_HTTPHEADER, s: "Accept: application/json")
-//        curlObject.setOption(CURLOPT_HTTPHEADER, s: "Cache-Control: no-cache")
-//        curlObject.setOption(CURLOPT_USERAGENT, s: "PerfectAPI2.0")
-//
-//        if !bearerToken.isEmpty {
-//            curlObject.setOption(CURLOPT_HTTPHEADER, s: "Authorization: Bearer \(bearerToken)")
-//        }
-//
-//        switch method {
-//        case .post :
-//            let byteArray = [UInt8](body.utf8)
-//            curlObject.setOption(CURLOPT_POST, int: 1)
-//            curlObject.setOption(CURLOPT_POSTFIELDSIZE, int: byteArray.count)
-//            curlObject.setOption(CURLOPT_COPYPOSTFIELDS, v: UnsafeMutablePointer(mutating: byteArray))
-//
-//            if encoding == "form" {
-//                curlObject.setOption(CURLOPT_HTTPHEADER, s: "Content-Type: application/x-www-form-urlencoded")
-//            } else {
-//                curlObject.setOption(CURLOPT_HTTPHEADER, s: "Content-Type: application/json")
-//            }
-//
-//        default: //.get :
-//            curlObject.setOption(CURLOPT_HTTPGET, int: 1)
-//        }
-//
-//
-//        var header = [UInt8]()
-//        var bodyIn = [UInt8]()
-//
-//        var code = 0
-//        var data = [String: Any]()
-//        var raw = [String: Any]()
-//
-//        var perf = curlObject.perform()
-//        defer { curlObject.close() }
-//
-//        while perf.0 {
-//            if let h = perf.2 {
-//                header.append(contentsOf: h)
-//            }
-//            if let b = perf.3 {
-//                bodyIn.append(contentsOf: b)
-//            }
-//            perf = curlObject.perform()
-//        }
-//        if let h = perf.2 {
-//            header.append(contentsOf: h)
-//        }
-//        if let b = perf.3 {
-//            bodyIn.append(contentsOf: b)
-//        }
-//        let _ = perf.1
-//
-//        // Parsing now:
-//
-//        // assember the header from a binary byte array to a string
-//        //		let headerStr = String(bytes: header, encoding: String.Encoding.utf8)
-//
-//        // parse the header
-//        //		let http = HTTPHeaderParser(header:headerStr!)
-//
-//        // assamble the body from a binary byte array to a string
-//        let content = String(bytes:bodyIn, encoding:String.Encoding.utf8)
-//
-//        // prepare the failsafe content.
-//        //		raw = ["status": http.status, "header": headerStr!, "body": content!]
-//
-//        // parse the body data into a json convertible
-//        do {
-//            if (content?.count)! > 0 {
-//                if (content?.starts(with: "["))! {
-//                    let arr = try content?.jsonDecode() as! [Any]
-//                    data["response"] = arr
-//                } else {
-//                    data = try content?.jsonDecode() as! [String : Any]
-//                }
-//            }
-//            return data
-//            //			return (http.code, data, raw, http)
-//        } catch {
-//            return [:]
-//            //			return (http.code, [:], raw, http)
-//        }
     }
 }
 
